ebeddc94fd39284d01bf78424d01bca37f4c3599
zen2see <zen2se@gmail.com>
2023-11-17 12:31:31 -0500

    UPDATED revenue and invoices to Suspense, connected to Vercel

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from "../../ui/skeletons";
+
+export default function Loading() {
+    // return <div className="text-2xl font-bold text-orange-600 italic">Loading...</div>
+    // OR SKELETONS
+    return <DashboardSkeleton />
+}
